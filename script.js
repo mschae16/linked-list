f371b217f16639158e9d0fcae9f43c83f5495bc5
Mike Marlow <michael.s.marlow@gmail.com>
2017-05-15 16:22:02 -0600

    JS update

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ $('.read-btn').on('click', function() {
   console.log('hello');
   $(this).toggleClass('.read');
 });
+
+//button enable on field input typed
+$(function () {
+  $('#website-title').keyup(function () {
+    if ($(this).val() == '') {
+      //Check to see if there is any text entered
+      // If there is no text within the input then disable the button
+          $('.enableOnInput').prop('disabled', true); }
+    else {
+      //If there is text in the input, then enable the button
+          $('.enableOnInput').prop('disabled', false);
+        }
+    });
+});
+//Grabbing text from entry fields once ENTER is pressed
+$('#enter-btn').on('click', function() {
+  $('.second-section').prepend(' <article>
+    <h2>The Website Title</h2>
+    <hr>
+    <a href="wwww.somewebsite.com" target="_blank">www.websiteurl.com</a>
+    <hr>
+    <div>
+    <button class="read-btn">Read</button>
+    <button class="delete-btn">Delete</button>
+    </div>
+  </article> ');
+
+})
